9e77f2e1c260cb37b27dace842e03d15d3b52d29
deltahedge1 <ihassan1@kpmg.com.au>
2022-08-20 17:45:23 +1000

    added jokes

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
+jokes = [
+    "Relationship status? I'll leave that in the database",
+    "What diet did the ghost developer go on? Boolean",
+    "How do you get the code from a bank vault? You checkout the their branch"
+]
+
 @app.get("/")
 def hello_world():
-    return "<p>hello world</p>"
+    joke = random.choice(jokes)
+    return joke
