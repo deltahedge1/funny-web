0ff51d51445ced24560de824df12abaca2f3bffd
deltahedge1 <ihassan1@kpmg.com.au>
2022-08-21 23:13:24 +1000

    refactored data import in app

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask
 from flask import render_template
 import random
 
-from data import jokes
+import data
 
 app = Flask(__name__)
 
 @app.get("/")
 def hello_world():
-    joke = random.choice(jokes)
+    joke = random.choice(data.jokes)
     return render_template('joke.html', joke_text=joke)
